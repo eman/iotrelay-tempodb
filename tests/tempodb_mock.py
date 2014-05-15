0dd8c9138855f65595988961c459b7bfc9595a71
Emmanuel Levijarvi <emansl@gmail.com>
2014-05-15 16:16:24 -0700

    Initial Commit: tests

---
+++ tests/tempodb_mock.py
@@ -0,0 +1,28 @@
+TIME_FMT = "%Y-%m-%d %H:%M:%S %z"
+
+class DataPoint(object):
+    def __init__(self, timestamp, value):
+        self.timestamp = timestamp
+        self.value = value
+
+    def __str__(self):
+        return "{0!s}, {1}".format(self.timestamp, self.value)
+
+    def __repr__(self):
+        return str(self)
+
+
+class Client(object):
+    data_points = []
+
+    def __init__(self, api_key, secret):
+        pass
+
+    @classmethod
+    def reset(cls):
+        cls.data_points = []
+
+    def write_key(self, series_key, data_points):
+        for point in data_points:
+            timestamp = point.timestamp.strftime(TIME_FMT)
+            self.data_points.append([timestamp, series_key, point.value])
---
+++ tests/tests.py
@@ -0,0 +1,46 @@
+import sys
+from datetime import datetime
+from unittest import mock, TestCase
+import csv
+import os
+from iotrelay import Reading
+from tests.tempodb_mock import Client, DataPoint
+import iotrelay_tempodb
+
+iotrelay_tempodb.Client = Client
+iotrelay_tempodb.DataPoint = DataPoint
+
+TIME_FMT = "%Y-%m-%d %H:%M:%S %z"
+TEST_DATA = os.path.join(os.path.realpath(os.path.dirname(__file__)),
+                         "test_data.csv")
+
+class IoTTempoDBTest(TestCase):
+    def readings(self):
+        try:
+            return self._readings
+        except AttributeError:
+            with open(TEST_DATA, 'rt') as f:
+                self._readings = [line for line in csv.reader(f)]
+        return self._readings
+
+    def compare_readings(self, num_readings):
+        config = {'batch size': num_readings, 'api key': '', 'api secret': ''}
+        Client.reset()
+        tdb_handler = iotrelay_tempodb.Handler(config)
+        for ts, key, value in self.readings():
+            timestamp = datetime.strptime(ts, TIME_FMT)
+            tdb_handler.set_reading(Reading('weather', value, timestamp, key))
+        tdb_handler.flush()
+        self.assertEqual(sorted(Client.data_points), sorted(self.readings()))
+
+    def test_compare_readings_batch_size_one(self):
+          config = {'batch size': 1, 'api key': '', 'api secret': ''}
+          Client.reset()
+          tdb_handler = iotrelay_tempodb.Handler(config)
+          for ts, key, value in self.readings():
+              timestamp = datetime.strptime(ts, TIME_FMT)
+              tdb_handler.set_reading(Reading('weather', value, timestamp, key))
+          self.assertEqual(Client.data_points, self.readings())
+
+    def test_compare_readings_vary_batch_size(self):
+        [self.compare_readings(i) for i in range(2, 12)]
